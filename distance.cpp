0d01b3f32652aaa90d21d171625c449c8f4d05b5
shanchenqi <shanchenqi@huawei.com>
2020-07-09 19:52:00 +0800

    delete 1 channel

--- distance.cpp
+++ distance.cpp
@@ -13,12 +13,12 @@ using namespace cv;
 
 #define PI  3.1415926535897
 
-double deltaE_cie76(const LAB& lab1,const LAB& lab2) {
-	
-	double lab_de76= sqrtl( powl(lab1.l-lab2.l, 2.0)+powl(lab1.a - lab2.a, 2.0)+ powl(lab1.b - lab2.b, 2.0));
+double deltaE_cie76(const LAB& lab1, const LAB& lab2) {
+
+	double lab_de76 = sqrtl(powl(lab1.l - lab2.l, 2.0) + powl(lab1.a - lab2.a, 2.0) + powl(lab1.b - lab2.b, 2.0));
 	return lab_de76;
-	
-	}
+
+}
 double to_rad(double degree)
 {
 	return degree * (PI / 180.0);
@@ -27,7 +27,7 @@ double to_deg(double rad) {
 	return ((180.0 / PI) * rad);
 }
 double deltacE_cmc(const LAB& lab1, const LAB& lab2, double kL = 1, double kC = 1) {
-	
+
 	double dL = lab2.l - lab1.l;
 	double da = lab2.a - lab1.a;
 	double db = lab2.b - lab1.b;
@@ -67,9 +67,9 @@ double deltacE_ciede94(const LAB& lab1, const LAB& lab2, int kH = 1, int kC = 1,
 	double sc = 1.0 + k1 * c1;
 	double sh = 1.0 + k2 * c1;
 	int sl = 1;
-	double res = powl(dl /(double) (kL * sl), 2) + powl(dc / (kC * sc), 2) + dh / powl(kH * sh, 2);
-	
-	return res > 0 ? sqrtl(res) : 0; 
+	double res = powl(dl / (double)(kL * sl), 2) + powl(dc / (kC * sc), 2) + dh / powl(kH * sh, 2);
+
+	return res > 0 ? sqrtl(res) : 0;
 
 }
 
@@ -105,10 +105,10 @@ double deltacE_ciede2000(const LAB& lab1, const LAB& lab2, double kL = 1.0, doub
 	if (labs(h1 - h2) <= to_rad(180)) {
 		H = (h1 + h2) / 2.0;
 	}
-	else if(labs(h1 - h2) > to_rad(180) && h1 + h2 < to_rad(360)){
-		 H = (h1 + h2 + to_rad(360)) / 2.0;
+	else if (labs(h1 - h2) > to_rad(180) && h1 + h2 < to_rad(360)) {
+		H = (h1 + h2 + to_rad(360)) / 2.0;
 	}
-	else{
+	else {
 		H = (h1 + h2 - to_rad(360)) / 2.0;
 	}
 
@@ -137,81 +137,45 @@ double deltacE_ciede2000(const LAB& lab1, const LAB& lab2, double kL = 1.0, doub
 	double sC = 1.0 + 0.045 * avg_c;
 	double sH = 1.0 + 0.015 * avg_c * (1.0 - 0.17 * cosl(H - to_rad(30)) + 0.24 * cosl(2.0 * H) + 0.32 * cosl(3.0 * H + to_rad(6.0)) - 0.2 * cosl(4.0 * H - to_rad(63.0)));
 	double sL = 1.0 + ((0.015 * powl(avg_l - 50.0, 2.0)) / sqrtl(20.0 + powl(avg_l - 50.0, 2.0)));
-	double rt = -2.0 * sqrt_c_pow * sinl(to_rad(60.0) * expl(-powl((H - to_rad(275.0) )/ to_rad(25.0), 2.0)));
+	double rt = -2.0 * sqrt_c_pow * sinl(to_rad(60.0) * expl(-powl((H - to_rad(275.0)) / to_rad(25.0), 2.0)));
 	double res = (powl(delta_L / (kL * sL), 2.0) + powl(delta_C / (kC * sC), 2.0) + powl(delta_H / (kH * sH), 2.0) + rt * (delta_C / (kC * sC)) * (delta_H / (kH * sH)));
 	return res > 0 ? sqrtl(res) : 0;
 }
 
 
-Mat distance_s(Mat Lab1, Mat Lab2,string distance) {
-	Mat distance_lab;
-	if (Lab1.channels() == 1) {
-		Mat distance_lab(Lab1.rows, 1, CV_64FC1);
-		
-		for (int i = 0; i < Lab1.rows; i++) {
-			double* datasrc1 = Lab1.ptr<double>(i);
-			double l1 = datasrc1[0];
-			double a1 = datasrc1[1];
-			double b1 = datasrc1[2];
-			double* datasrc2 = Lab2.ptr<double>(i);
-			double l2 = datasrc2[0];
-			double a2 = datasrc2[1];
-			double b2 = datasrc2[2];
+Mat distance_s(Mat Lab1, Mat Lab2, string distance) {
+	Mat distance_lab(Lab1.rows, Lab1.cols, CV_64FC1);
+	for (int i = 0; i < Lab1.rows; i++) {
+		for (int j = 0; j < Lab1.cols; j++) {
+			double l1 = Lab1.at<Vec3d>(i, j)[0];
+			double a1 = Lab1.at<Vec3d>(i, j)[1];
+			double b1 = Lab1.at<Vec3d>(i, j)[2];
+			double l2 = Lab2.at<Vec3d>(i, j)[0];
+			double a2 = Lab2.at<Vec3d>(i, j)[1];
+			double b2 = Lab2.at<Vec3d>(i, j)[2];
+			double L, a, b;
 			LAB lab1 = { l1,a1,b1 };
 			LAB lab2 = { l2,a2,b2 };
-			
 			if (distance == "distance_de94") {
-				distance_lab.at<double>(i, 0) = deltacE_ciede94(lab1, lab2);
+				distance_lab.at<double>(i, j) = deltacE_ciede94(lab1, lab2);
 			}
 			else if (distance == "distance_de76") {
-				
-				distance_lab.at<double>(i, 0) = deltaE_cie76(lab1, lab2);
+				distance_lab.at<double>(i, j) = deltaE_cie76(lab1, lab2);
 			}
 			else if (distance == "distance_cmc") {
-				distance_lab.at<double>(i, 0) = deltacE_cmc(lab1, lab2);
+				distance_lab.at<double>(i, j) = deltacE_cmc(lab1, lab2);
 			}
 			else if (distance == "distance_de00") {
-				distance_lab.at<double>(i, 0) = deltacE_ciede2000(lab1, lab2);
+				distance_lab.at<double>(i, j) = deltacE_ciede2000(lab1, lab2);
 			}
 			else {
 				cout << "wrong distance type" << endl;
 			}
 		}
-		return distance_lab;
+
+   
 	}
-	else if (Lab1.channels() == 3) {
-		Mat distance_lab(Lab1.rows, Lab1.cols, CV_64FC1);
-		for (int i = 0; i < Lab1.rows; i++) {
-			for (int j = 0; j < Lab1.cols; j++) {
-				double l1 =	Lab1.at<Vec3d>(i, j)[0];
-				double a1 = Lab1.at<Vec3d>(i, j)[1];
-				double b1 = Lab1.at<Vec3d>(i, j)[2];
-				double l2 = Lab2.at<Vec3d>(i, j)[0];
-				double a2 = Lab2.at<Vec3d>(i, j)[1];
-				double b2 = Lab2.at<Vec3d>(i, j)[2];
-				double L, a, b;
-				LAB lab1 = { l1,a1,b1 };
-				LAB lab2 = { l2,a2,b2 };
-				if (distance == "distance_de94") {
-					distance_lab.at<double>(i, j) = deltacE_ciede94(lab1, lab2);
-				}
-				else if (distance == "distance_de76") {
-					distance_lab.at<double>(i, j) = deltaE_cie76(lab1, lab2);
-				}
-				else if (distance == "distance_cmc") {
-					distance_lab.at<double>(i, j) = deltacE_cmc(lab1, lab2);
-				}
-				else if (distance == "distance_de00") {
-					distance_lab.at<double>(i, j) = deltacE_ciede2000(lab1, lab2);
-				}
-				else {
-					cout << "wrong distance type" << endl;
-				}
-			}
-		}
-		return distance_lab;
-	}
-	
+	return distance_lab;
 }
 
 
