b494e37952ecd78582fdd1ee79950c310a645eca
shanchenqi <shanchenqi@huawei.com>
2020-08-25 20:13:05 +0800

    new v4

---
+++ operations.hpp
@@ -0,0 +1,75 @@
+#ifndef Operations_H
+#define Operations_H
+#include <functional>
+#include <vector>
+#include "opencv2\core\core.hpp"
+#include "utils.hpp"
+
+namespace cv {
+namespace ccm {
+
+typedef std::function<Mat(Mat)> MatFunc;
+
+class Operation {
+public:
+	bool linear;
+	cv::Mat M;
+	MatFunc f;
+	Operation() : linear(true), M(Mat()) {};
+	//Operation(Mat M) :linear(true), M{ M } {};
+	Operation(Mat M) :linear(true), M( M ) {};
+	Operation(MatFunc f) : linear(false), f(f) {};
+	Mat operator()(Mat& abc) {
+		if (!linear) { return f(abc); }
+		if (M.empty()) { return abc; }
+		return multiple(abc, M);
+	};
+	void add(Operation& other) {
+		if (M.empty()) {
+			M = other.M.clone();
+		}
+		else {
+			M = M * other.M;
+		}
+	};
+	void clear() {
+		M = Mat();
+		
+	};
+};
+
+Operation identity_op( [](Mat x) {return x; } );
+
+class Operations {
+public:
+	std::vector<Operation> ops;
+	Operations() :ops{ } {};
+	Operations(std::initializer_list<Operation> op) :ops{ op } {};
+	Operations& add(const Operations& other) {
+		ops.insert(ops.end(), other.ops.begin(), other.ops.end());
+		return *this;
+	};
+	Mat run(Mat abc) {
+		Operation hd;
+		for (auto& op : ops) {
+			if (op.linear) {
+				hd.add(op);
+			}
+			else {
+				abc = hd(abc);
+				hd.clear();
+				abc = op(abc);
+			}
+		}
+		abc = hd(abc);
+		hd.clear();
+		return abc;
+	};
+};
+
+Operations identity{ identity_op };
+}
+}
+
+#endif
+
